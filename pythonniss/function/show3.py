d0b8138c0f0b386e60e4beaeea98c53253c01cc8
Biswaranjani Biswal <biswalbiswaranjani60.com>
2026-03-01 10:03:53 +0530

    function

---
+++ pythonniss/function/show3.py
@@ -0,0 +1,6 @@
+def show(a):
+	print("hi")
+	return a*3
+print("bye",show(5))
+print(f"bye {show(5)} ok")
+print(f"bye {2*3+5} ok")
--- pythonniss/function/sical4.py
+++ pythonniss/function/sical4.py
@@ -1,10 +1,11 @@
 def sical(p,r,t):
-	SI=(p*r*t)/100
-	print("simple intrest=",SI)
+	si=(p*r*t)/100
+	return si
 print("enter principal")
 p=float(input())
 print("enter rate")
 r=float(input())
 print("enter time")
 t=float(input())
-sical(p,r,t)
+res=sical(p,r,t)
+print("simpleintrest=",res)
