f05c604fe13a52f054a69f8798571990aa487c42
Biswaranjani Biswal <biswalbiswaranjani60.com>
2026-03-30 20:00:29 +0530

    patterprogram

---
+++ pythonniss/patterprogram/patternprogrm4.py
@@ -0,0 +1,5 @@
+for i in range(65,68,1):
+	for j in range(65,69,1):
+		print(chr(j),end="\t")
+	print()
+	
---
+++ pythonniss/patterprogram/rev3.py
@@ -0,0 +1,4 @@
+for i in range(1, 5):
+    for j in range(4, 4-i, -1):
+        print(chr(64 + j), end=" ")
+    print()
